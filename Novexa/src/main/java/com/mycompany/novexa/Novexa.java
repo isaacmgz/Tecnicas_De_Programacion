c7736999cfda8bc47ab0a146751c0e181cc3dd17
isaacmgz <isaac.mesag@udea.edu.co>
2025-04-06 17:17:08 -0500

    Agregar archivos

---
+++ Novexa/src/main/java/com/mycompany/novexa/Novexa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.novexa;
+
+/**
+ *
+ * @author isaacmgz
+ */
+public class Novexa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
